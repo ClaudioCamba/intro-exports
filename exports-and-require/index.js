969e43f046bd5ca6d8c04bdc63d631123d3c33ff
Claudio Camba <claudiocamba@Claudios-iMac.local>
2023-11-28 15:10:26 +0000

    Completed challenge 1

--- exports-and-require/index.js
+++ exports-and-require/index.js
@@ -1,8 +1,8 @@
-const somethingFromFile1 = require('./examples/file-1');
-const somethingFromFile2 = require('./examples/file-2');
-const somethingFromFile3 = require('./examples/file-3');
-const somethingFromFile4 = require('./examples/file-4');
-const somethingFromFile5 = require('./examples/file-5');
-const somethingFromFile6 = require('./examples/file-6');
+const somethingFromFile1 = require('./examples/file-1'); // "Vel"
+const somethingFromFile2 = require('./examples/file-2'); // { tutor: Vel }
+const somethingFromFile3 = require('./examples/file-3'); // [Function: printHello]
+const somethingFromFile4 = require('./examples/file-4'); // { add: [Function: add] }
+const somethingFromFile5 = require('./examples/file-5'); // { printHello: [Function: printHello], add: [Function: add] }
+const somethingFromFile6 = require('./examples/file-6'); // {}
 
-//console.log(REPLACE_ME_WITH_A_VARIABLE_FROM_ABOVE);
+console.log(somethingFromFile6);
